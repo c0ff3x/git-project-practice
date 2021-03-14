678fbf3cfbe0682078fd9a0dc5e16707c0835140
Oscar-MoralesB <oscar.moraless@protonmail.com>
2021-03-14 16:14:30 -0600

    Added class to read files and serch the contents inside a directory, this class would only work inside the dirs and over its contents

---
+++ ReadFiles.java
@@ -0,0 +1,27 @@
+/*
+  This class reads a path a return its kind ofcontent, it detects what kind
+  of files the directory has and return the name of the dir with the types 
+  of its content (if the dir has another dirs, the class would return the
+  the type of files that the fir has with the name of its parent dir).
+  The format to return is:
+  name_directory: [type_content1, type_content2: type_content3]
+  If the directory has others directories inside it, then the format returned
+  is:
+  name_directory:
+  	name_directory: [type_content1, type_content2, type_content3]
+  	name_directory: [type_content1, type_content2, type_content3]
+  		name_directory: [type_content1, type_content2, type_content3]
+  		name_directory: [type_content1, type_content2, type_content3]
+  	name_directory: [type_content1, type_content2, type_content3]
+*/
+public class ReadFiles {
+	private String path = null;
+
+	public ReadFiles(String readPath){
+		path = readPath;
+	}
+
+	public String getPath(){
+		return path;
+	}
+}
